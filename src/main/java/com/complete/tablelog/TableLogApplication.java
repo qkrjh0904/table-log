44cac1761c658994172a5c083696248b4e9b1925
jeongho <qkrjh0904@gmail.com>
2023-05-22 21:28:43 +0900

    init

---
+++ src/main/java/com/complete/tablelog/TableLogApplication.java
@@ -0,0 +1,13 @@
+package com.complete.tablelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/complete/tablelog/TableLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complete.tablelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
